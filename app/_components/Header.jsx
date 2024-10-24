1c19f2f7d01f3d17bb05c32bf6f0d5917945215b
Ahmed Gomaa <ahmedfathy.gomaa@outlook.com>
2024-10-24 12:53:47 +0300

    hero_modifiy

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Image from 'next/image';
 function Header() {
   return (
     <div>
@@ -61,6 +61,19 @@ function Header() {
             </a>
           </div>
         </div>
+        <div className="hidden md:flex items-center">
+                {/* Egypt flag icon and language selector */}
+                <Image
+                  src="/images/egypt-flag.png" // Add the flag image in the public/images folder
+                  alt="Egypt Flag"
+                  width={24}
+                  height={24}
+                  className="rounded-full"
+                />
+                <a href="#" className="ml-2 text-gray-500 hover:text-gray-700 text-sm">
+                  العربية
+                </a>
+              </div>
 
         <div className="block md:hidden">
           <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
 */}
 
 <section
-  className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat"
+  className="w-[full] h-[700px] relative bg-[url(https://realestate.eg/ckfinder/userfiles/images/Mivida/Mivida-Emaar%20Misr.jpg)] bg-cover bg-center bg-no-repeat"
 >
   <div
     className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
