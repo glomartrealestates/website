552ae34828d895b1a8ebaa6798fb142f0939ec5b
AmiraAli1612001 <programming.tracks@gmail.com>
2024-11-15 00:10:52 +0200

    area

--- app/(pages)/home/page.jsx
+++ app/(pages)/home/page.jsx
@@ -10,7 +10,7 @@ import Offer from "../../components/offer/page"
 
 import { properties } from "./data"
 import { Grid } from '@mui/material'
-
+import {area} from "./data"
 export default function Page() {
     let icon_paper_h1 = "What Are You Looking For ?"
     let top_area = "Top Areas"
@@ -22,134 +22,52 @@ export default function Page() {
             img: "https://prod-images.cooingestate.com/processed/pathway/image/1/medium.webp",
             title: "Developer Units",
             width: 40,
+            link : "#",
         },
         {
             img: "https://prod-images.cooingestate.com/processed/pathway/image/2/medium.webp",
             title: "Resale Units",
-            width: 40
+            width: 40,
+            link : "#",
+
 
         },
         {
             img: "https://prod-images.cooingestate.com/processed/pathway/image/3/medium.webp",
             title: "Sell Your Unit",
-            width: 40
+            width: 40,
+            link : "#",
+
 
         },
         {
             img: "https://prod-images.cooingestate.com/processed/pathway/image/4/medium.webp",
             title: "Nawy Now",
-            width: 40
+            width: 40,
+            link : "#",
+
 
         },
         {
             img: "https://prod-images.cooingestate.com/processed/pathway/image/5/medium.webp",
             title: "Offers",
-            width: 40
+            width: 40,
+            link : "#",
+
 
         },
         {
             img: "https://prod-images.cooingestate.com/processed/pathway/image/6/medium.webp",
             title: "Top Areas",
-            width: 40
+            width: 40,
+            link : "/home/area",
+
 
         },
     ]
 
 
-    let area = [
-        {
-            img: "https://prod-images.cooingestate.com/processed/area/image/2/high.webp",
-            title: "New Cairo",
-            number: 12,
-            number2: 24,
-            text2: "Properties Available",
-            compounds: "Compounds",
-            round: "full",
-            width: 80,
-
-
-        },
-        {
-            img: "https://prod-images.cooingestate.com/processed/area/image/1/high.webp",
-            title: "6th of October City",
-            number: 12,
-            number2: 24,
-            text2: "Properties Available",
-            compounds: "Compounds",
-            round: "full", width: 80,
-
-
-        },
-        {
-            img: "https://prod-images.cooingestate.com/processed/area/image/9/high.webp",
-            title: "New Capital City",
-            number: 12,
-            number2: 24,
-            text2: "Properties Available",
-            compounds: "Compounds",
-            round: "full"
-            , width: 80,
-
-        },
-        {
-            img: "https://prod-images.cooingestate.com/processed/area/image/10/high.webp",
-            title: "Mostakbal City",
-            number: 12,
-            number2: 24,
-            text2: "Properties Available",
-            compounds: "Compounds",
-            round: "full",
-            width: 80,
-
-
-        },
-        {
-            img: "https://prod-images.cooingestate.com/processed/area/image/4/high.webp",
-            title: "Ain Sokhna",
-            number: 12,
-            number2: 24,
-            text2: "Properties Available",
-            compounds: "Compounds",
-            round: "full",
-            width: 80,
-
-
-        },
-        {
-            img: "https://prod-images.cooingestate.com/processed/area/image/3/high.webp",
-            title: "North Coast - Sahel ",
-            number: 12,
-            number2: 24,
-            text2: "Properties Available",
-            compounds: "Compounds",
-            round: "full",
-            width: 80,
-
-
-        },
-        {
-            img: "https://prod-images.cooingestate.com/processed/area/image/5/high.webp",
-            title: "El Gouna",
-            number: 12,
-            number2: 24,
-            text2: "Properties Available",
-            compounds: "Compounds",
-            round: "full",
-            width: 80,
-
-
-        }, {
-            img: "https://prod-images.cooingestate.com/processed/compound_image/image/1078/high.webp",
-            title: "New Heliopolis",
-            number: 12,
-            number2: 24,
-            text2: "Properties Available",
-            compounds: "Compounds",
-            round: "full",
-            width: 80,
-
-        },
-    ]
+   
     let data = [
         {
             img: "https://prod-images.cooingestate.com/processed/compound_image/image/2023/low.webp",
